bc7db0d17bc8f5397aaefb15b3ef8070a98972de
dnfheh <dnfheh@naver.com>
2024-01-14 21:46:45 +0900

    jpashop실무

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {System.out.println("Hello");SpringApplication.run(JpashopApplication.class, args);
+	public static void main(String[] args) {System.out.println("Hello2");SpringApplication.run(JpashopApplication.class, args);
 	}
 
 }
